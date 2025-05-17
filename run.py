912debd710d445e6382d2b5c3fb9ece59e8f280c
Khushi Tiwari <tkhushi0519@gmail.com>
2025-05-17 23:03:23 +0530

    Fix database migration strategy

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
-# run.py
-from app import create_app
+from app import create_app, db
+from app.models import user, goal
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+@app.cli.command("init-db")
+def init_db():
+    with app.app_context():
+        db.create_all()
+
+if __name__ == "__main__":
+    app.run()
